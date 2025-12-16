778eba50f13199a27d0307b1ce71c6b4770436c1
sze4konpal-glitch <sze4konpal@vasvari.org>
2025-12-16 17:54:24 +0100

    kp_LoginHomeP_hozzá_adása

--- Konyvforum/src/App.vue
+++ Konyvforum/src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
   import NavBar from './components/NavBar.vue';
+  import LoginHomeP from './components/LoginHomeP.vue';
 </script>
 
 <template>
 <NavBar/>
+<div class="page-content">
+    <LoginHomeP/>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page-content {
+  /* Mivel a navbar 60px magas, adjunk neki 80px helyet fentről */
+  padding-top: 80px; 
+}
+</style>
---
+++ Konyvforum/src/components/LoginHomeP.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <div class="dev">
+        <h1>Ez a fő oldal</h1>
+    </div>
+</template>
